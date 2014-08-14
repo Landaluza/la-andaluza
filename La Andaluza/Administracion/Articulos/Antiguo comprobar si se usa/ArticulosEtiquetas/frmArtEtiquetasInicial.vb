610d80171a7eea0fb35331064d6cbbc37ccad5bb
miguel_vega_menacho <miguel.vega.menacho@gmail.com>
2014-08-14 11:49:36 +0200

    limpieza, borrados algunos formularios

--- La Andaluza/Administracion/Articulos/Antiguo comprobar si se usa/ArticulosEtiquetas/frmArtEtiquetasInicial.vb
+++
@@ -1,8 +0,0 @@
-﻿Imports BasesParaCompatibilidad.ComboBoxExtension
-
-Public Class frmArtEtiquetasInicial
-
-    Private Sub frmArtEtiquetasInicial_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-        cboEtiquetasTipos.mam_DataSource("ArticulosEtiquetasTiposCbo", False)
-    End Sub
-End Class
